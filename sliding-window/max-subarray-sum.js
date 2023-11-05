a7f26ffb03adaec98653350ff5ee931809b99197
Vitaliy Boyanivskyy <vitaliy.boyanivsky@scalors.com>
2023-11-05 12:16:18 +0200

    sliding window and restructuring files into foldesr

---
+++ sliding-window/max-subarray-sum.js
@@ -0,0 +1,25 @@
+/**
+ * function which returns sum of subarray of given length
+ *
+ * @param arr
+ * @param n - sub array length
+ */
+function maxSubarraySum(arr, num) {
+	let maxSum = 0;
+	if (arr.length < num) return null;
+	for (let i = 0; i < num; i++) {
+		maxSum += arr[i];
+	}
+
+	let tempSum = maxSum;
+	for (let i = num; i < arr.length; i++) {
+		tempSum = tempSum - arr[i - num] + arr[i];
+		maxSum = Math.max(maxSum, tempSum);
+	}
+
+	return maxSum;
+}
+
+console.log(maxSubarraySum([1, 2, 3, 1, 5, 2, 1], 2)); // 7
+console.log(maxSubarraySum([1, 2, 3, 1, 5, 2, 1], 3)); // 9
+console.log(maxSubarraySum([1, 2, 3, 1, 5, 2, 1], 1)); // 5
